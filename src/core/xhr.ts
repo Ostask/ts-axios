810f16214373d809d08dba7476212547417be609
Ostask <502724802@qq.com>
2020-05-15 18:17:19 +0800

    111

---
+++ src/core/xhr.ts
@@ -0,0 +1,69 @@
+import { AxiosRequestConfig,AxiosPromise ,AxiosResponse} from '../types'
+import {parseHeaders} from '../helpers/headers'
+import {createError} from '../helpers/error'
+
+export default function xhr(config:AxiosRequestConfig):AxiosPromise {
+    return new Promise((resolve,reject)=>{
+        const {data = null,url,method='get',headers,responseType,timeout} = config
+
+        const request = new XMLHttpRequest()
+
+        if(responseType){
+            request.responseType = responseType
+        }
+
+        if(timeout){
+            request.timeout = timeout
+        }
+    
+        request.open(method.toLocaleUpperCase(),url!,true)
+
+        request.onreadystatechange = function handleLoad(){
+            if(request.readyState !== 4) {
+                return
+            }
+
+            if(request.status === 0){
+                return
+            }
+
+            const responsHeaders = parseHeaders(request.getAllResponseHeaders())
+            const responseData = responseType !== 'text'?request.response : request.responseText
+            const response:AxiosResponse = {
+                data:responseData,
+                status:request.status,
+                statusText:request.statusText,
+                headers:responsHeaders,
+                config,
+                request
+            }
+            handleResponse(response)
+        }
+
+        request.onerror = function handleError(){
+            reject(createError('network Error',config,null,request))
+        }
+
+        request.ontimeout = function handleTimeOut(){
+            reject(createError(`Timeout of ${timeout} ms exceeded`,config,'ECONNABORTED',request))
+        }
+    
+        Object.keys(headers).forEach((name) => {
+            if(data === null && name.toLowerCase() === 'content-type'){
+                delete headers[name]
+            }else{
+                request.setRequestHeader(name,headers[name])
+            }
+        })
+    
+        request.send(data)
+
+        function handleResponse(response:AxiosResponse):void{
+            if(response.status >= 200 && response.status < 300){
+                resolve(response)
+            }else{
+                reject(createError(`Request failed width status code ${response.status}`,config,null,request,response))
+            }
+        }
+    })
+}
